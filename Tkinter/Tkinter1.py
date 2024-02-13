70e6fdb6aa43a9481b41d86ecd93da8b782df60f
AnkurKonan <112815485+AnkurKonan@users.noreply.github.com>
2024-02-13 17:06:05 +0530

    Add files via upload

---
+++ Tkinter/Tkinter1.py
@@ -0,0 +1,11 @@
+from tkinter import *
+import tkinter
+
+root = tkinter.Tk()
+root.title("Ankur Konan")
+root.geometry("500x500")
+root.maxsize(400, 400)
+root.minsize(300, 300)
+root.mainloop()
+
+# this is just basic format of GUI (graphical user interface) in python which I was practicing
