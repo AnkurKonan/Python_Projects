69b2a77d8ca7ccd4a3d3b1208b12a84189620cf0
AnkurKonan <112815485+AnkurKonan@users.noreply.github.com>
2024-03-07 20:49:13 +0530

    Create Tkinter3.py

---
+++ Tkinter/Practice/Tkinter3.py
@@ -0,0 +1,13 @@
+from tkinter import *
+import tkinter
+
+root = tkinter.Tk()
+root.title("Ankur Konan")
+root.geometry("250x250")
+# method is used to set icon at top of GUI
+root.iconbitmap("coin.jpeg")
+# setting up color of GUI
+root["bg"] = "Red"
+root.mainloop()
+
+# by the way this coin image i have made in krita (free alternative of adobe photoshop)
