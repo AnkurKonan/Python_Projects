109366ae0e42164e169807c743f8cafaa067584c
AnkurKonan <112815485+AnkurKonan@users.noreply.github.com>
2024-03-08 16:05:12 +0530

    Update Center-button.py

--- Tkinter/Practice/Center-button.py
+++ Tkinter/Practice/Center-button.py
@@ -1,8 +1,12 @@
 from tkinter import *
+
 root = Tk()
-def click():
-	label1 = Label(root, text="Button Clicked")
-	label1.pack()
-button = Button(root, text="Click Me", command=click)
-button.pack()
+root.title('Center button')
+root.geometry("500x500")
+button_1 = Button(root, text="Button 1", font=("Helvetica", 20))
+button_2 = Button(root, text="Button 2", font=("Helvetica", 20))
+button_1.grid(column=0, row=0)
+button_2.grid(column=1, row=0)
+my_button = Button(root, text="Click Me", font=("Helvetica, 20"))
+my_button.place(relx=0.5, rely=0.5, anchor=CENTER)
 root.mainloop()
