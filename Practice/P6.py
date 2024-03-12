dafb21d489904313d3a603ef655ce8c0c9f2a965
AnkurKonan <112815485+AnkurKonan@users.noreply.github.com>
2024-03-12 22:33:45 +0530

    Create P6.py

---
+++ Practice/P6.py
@@ -0,0 +1,17 @@
+# "Ankur"
+# 'Ankur'
+# name = "Ankur"
+# name +=" is my name"
+# age = str(39)
+# print(name)
+
+print("""Ankur
+is 
+best
+programmer""")
+print("Ankur".upper())
+print("Ankur".lower())
+print("Ankur is 19 years old".title())
+print("Ankur".islower())
+print("Ankur".isdigit())
+print("Ankur".isalnum())
