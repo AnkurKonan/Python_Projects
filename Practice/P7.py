d27f04f0ac2a99b9287273a80c002a7785221813
AnkurKonan <112815485+AnkurKonan@users.noreply.github.com>
2024-03-12 22:56:44 +0530

    Update P7.py

--- Practice/P7.py
+++ Practice/P7.py
@@ -7,3 +7,10 @@ milk = True
 choclate = False
 ready_to_drink = all([milk, choclate])
 print(ready_to_drink)
+
+# num1 = 56;
+# num2 = complex(2,3)
+# print(num2.real, num2.imag)
+
+print(abs(-5.5))
+print(round(5.89, 1))
