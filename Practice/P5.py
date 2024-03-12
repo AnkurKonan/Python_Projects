7f7ecd52ac64fd8585abf7358a6efc8643f684e5
AnkurKonan <112815485+AnkurKonan@users.noreply.github.com>
2024-03-12 22:11:43 +0530

    Create P5.py

---
+++ Practice/P5.py
@@ -0,0 +1,14 @@
+#or
+print(0 or 1)
+print(False or 'hey')
+print('hi' or 'hey')
+print([] or False)
+print(False or [])
+
+#and
+print(0 and 1)
+print(1 and 0)
+print(False and 'hey')
+print('hi' and 'hey')
+print([] and False)
+print(False and [])
