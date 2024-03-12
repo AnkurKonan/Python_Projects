7993d121d0714b3d23a0ba7eeadeea7bce9ad1ca
AnkurKonan <112815485+AnkurKonan@users.noreply.github.com>
2024-03-12 22:48:42 +0530

    Create P7.py

---
+++ Practice/P7.py
@@ -0,0 +1,9 @@
+# gas1 = True
+# gas2 = False
+# measure_any_gas = any([gas1, gas2])
+# print(measure_any_gas)
+
+milk = True
+choclate = False
+ready_to_drink = all([milk, choclate])
+print(ready_to_drink)
