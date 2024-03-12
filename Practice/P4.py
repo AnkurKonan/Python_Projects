b773ec8b5cace6b949a486c09dd1b1d942a7bc33
AnkurKonan <112815485+AnkurKonan@users.noreply.github.com>
2024-03-12 20:20:23 +0530

    Create P4.py

---
+++ Practice/P4.py
@@ -0,0 +1,29 @@
+#Arithmatic operators
+age = 8
+age += 8
+print(age)
+age -= 8
+print(age)
+age *= 8
+print(age)
+age /= 8
+print(age)
+age %= 8
+print(age)
+
+#comparison operator
+a = 1
+b = 2
+print(a == b)
+print(a != b)
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
+
+#boolean operators
+condition1 = True
+condition2 = False
+print(not condition1)
+print(condition1 and condition2)
+print(condition1 or condition2)
