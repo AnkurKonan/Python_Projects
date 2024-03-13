c44311c039babd7b5b74c565dcb2109b28d12784
AnkurKonan <112815485+AnkurKonan@users.noreply.github.com>
2024-03-13 18:15:19 +0530

    Update P8.py

--- Practice/P8.py
+++ Practice/P8.py
@@ -1,3 +1,4 @@
+#Enums
 from enum import Enum
 class State(Enum):
     INACTIVE = 0
@@ -8,12 +9,13 @@ print(State['ACTIVE'])
 print(list(State))
 print(len(State))
 
-number1 = int(input("Enter 1st number: "))
-number2 = int(input("Enter 2nd number: "))
-number3 = input("Enter 3rd number: ")
-number4 = input("Enter 4th number: ")
+#Inputs
+# number1 = int(input("Enter 1st number: "))
+# number2 = int(input("Enter 2nd number: "))
+# number3 = input("Enter 3rd number: ")
+# number4 = input("Enter 4th number: ")
 
-numbers_concatenated = number3 + number4
-numbers_added = number1 + number2
-print(numbers_added)
-print(numbers_concatenated)
+# numbers_concatenated = number3 + number4
+# numbers_added = number1 + number2
+# print(numbers_added)
+# print(numbers_concatenated)
