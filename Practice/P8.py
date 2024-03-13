323d7936287cf277eaeb25aa17ac356cb7452d50
AnkurKonan <112815485+AnkurKonan@users.noreply.github.com>
2024-03-13 18:14:44 +0530

    Update P8.py

--- Practice/P8.py
+++ Practice/P8.py
@@ -7,3 +7,13 @@ print(State(1))
 print(State['ACTIVE'])
 print(list(State))
 print(len(State))
+
+number1 = int(input("Enter 1st number: "))
+number2 = int(input("Enter 2nd number: "))
+number3 = input("Enter 3rd number: ")
+number4 = input("Enter 4th number: ")
+
+numbers_concatenated = number3 + number4
+numbers_added = number1 + number2
+print(numbers_added)
+print(numbers_concatenated)
