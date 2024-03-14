2f9e65eaebbe19c937f88bccf2ac80803e563ef8
AnkurKonan <112815485+AnkurKonan@users.noreply.github.com>
2024-03-14 14:19:57 +0530

    Update PTen.py

--- Practice/PTen.py
+++ Practice/PTen.py
@@ -13,3 +13,27 @@ def hello(name):
         return
     print("Hello " + name)
 hello(False)
+
+age = 19
+def test():
+    print(age)
+print(age)
+test()
+
+def talk(phrase):
+    def say(word):
+        print(word)
+
+    words = phrase.split(' ')
+    for word in words:
+        say(word)
+talk("I am going to buy milk")
+
+def count():
+    count = 0
+    def increment():
+        nonlocal count
+        count = count + 1
+        print(count)
+    increment()
+count()
