b4644f236bf4014cfdb0c1a242efe9a027ace311
AnkurKonan <112815485+AnkurKonan@users.noreply.github.com>
2024-03-16 12:34:57 +0530

    Update P16.py

--- Practice/P16.py
+++ Practice/P16.py
@@ -2,3 +2,24 @@ from functools import reduce
 expense = [('Ankur', 60), ('Rahul', 80)]
 sum = reduce(lambda a, b: a[1] + b[1], expense)
 print(sum)
+
+#Recussion
+# def factorial(n):
+#     if n == 1: return 1
+#     return n * factorial(n-1)
+# print(factorial(3))
+# print(factorial(4))
+# print(factorial(5))
+
+#Decorators
+def logtime(func):
+    def wrapper():
+        print("before")
+        val = func()
+        print("after")
+        return val
+    return wrapper
+@logtime
+def hello():
+    print("hello")
+hello()
