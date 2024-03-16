51f4119dc1f9ac325958e193bf4b6b3c27625e25
AnkurKonan <112815485+AnkurKonan@users.noreply.github.com>
2024-03-16 14:40:08 +0530

    Update P17.py

--- Practice/P17.py
+++ Practice/P17.py
@@ -2,3 +2,12 @@
 def increment(n: int) -> int:
     return n + 1
 count: int = 0
+
+#Exception
+try:
+    result = 2/0
+except ZeroDivisionError:
+    print("Cannot divide by zero")
+finally:
+    result = 1
+print(result)
