8f0c8d6e71cf60fcd02583f36b56ecce1b512e0e
Ankur Konan <112815485+AnkurKonan@users.noreply.github.com>
2024-03-31 12:33:25 +0530

    Create P20.py

---
+++ Practice/P20.py
@@ -0,0 +1,45 @@
+greeting = "good morning "
+name = "ankur"
+c = greeting + name
+d = "Ankurisaniceprogrammer"
+a = "I started learing coding when I was in class 12"
+print(greeting + name)
+print(c[4])
+print(c[0:4])
+print(c[1:2])
+print(c[1:3])
+print(c[1:])
+print(c[-4:-1])
+print(d[0::3])
+print(d[0:-1])
+
+print(len(a))
+print(a.count("a"))
+print(a)
+print(a.capitalize())
+print(a.upper())
+print(a.lower())
+print(a.find("I"))
+print(a.find("was"))
+print(a.endswith("10"))
+print(a.startswith("Ankur"))
+
+a = [3,4,5,6,7]
+print(a[0] + a[1] + a[2] + a[3])
+print(sum(a))
+
+Avenger1 = {
+    "name": "SpiderMan",
+    "powers": "super-human strength of spider",
+    "age": 20,
+    "weapons": ["web-shooters", "suit"],
+    "family": {"antmay", "MJ"},
+    "weakness": ("antmay"),
+}
+print(Avenger1["name"])
+print(Avenger1["powers"])
+print(Avenger1["age"])
+print(Avenger1["family"])
+print(Avenger1["weakness"])
+# print(Avenger1["family"]["antmay"])
+
