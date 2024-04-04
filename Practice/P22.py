c3a896464c415eaab4256f05be1c348b4204766d
Ankur Konan <112815485+AnkurKonan@users.noreply.github.com>
2024-04-04 16:51:53 +0530

    Create P22.py

---
+++ Practice/P22.py
@@ -0,0 +1,18 @@
+num1 = int(input("Enter the number: \n"))
+num2 = int(input("Enter the number: \n"))
+num3 = int(input("Enter the number: \n"))
+sum = {num1 + num2 + num3}
+print(sum)
+
+sum2 = {17, "17", 17.2}
+print(sum2)
+print(len(sum2))
+
+fav_pokemon = {}
+a = input("Enter your favorite pokemon: ")
+b = input("Enter your favorite pokemon: ")
+c = input("Enter your favorite pokemon: ")
+fav_pokemon['charmendar'] = a
+fav_pokemon['bulbasor'] = b
+fav_pokemon['squatal'] = c
+print(fav_pokemon)
