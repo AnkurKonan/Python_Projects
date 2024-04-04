58155afc4031c0af68b69a348d8f8e1f753c4050
Ankur Konan <112815485+AnkurKonan@users.noreply.github.com>
2024-04-04 17:02:51 +0530

    Create P23.py

---
+++ Practice/P23.py
@@ -0,0 +1,11 @@
+for i in range(1,40,5):
+    print(i)
+for i in range(100):
+    print(i)
+for i in range(1000):
+    print(i)
+
+# for i in range(1000000):
+#     print(i)
+# for i in range(1000000000):  #don't run this you system might crash
+#     print(i)
