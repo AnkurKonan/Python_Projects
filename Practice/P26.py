3b5b15fdd554a3489016e778dffd9ff45be63e40
Ankur Konan <112815485+AnkurKonan@users.noreply.github.com>
2024-04-05 21:34:31 +0530

    Create P26.py

---
+++ Practice/P26.py
@@ -0,0 +1,20 @@
+def percent(marks):
+    p = ((marks[0] + marks[1] + marks[2] + marks[3] + name[4])/500)*100
+    return p
+mark1 = [80, 90, 70, 80, 85]
+(percent(mark1))
+
+def default_weapon(weapon = "Punch"):
+    print("Use " + weapon)
+default_weapon("Sword")
+default_weapon("Shot Gun")
+default_weapon("Blade")
+default_weapon("Machine Gun")
+default_weapon()
+
+n = 12
+product = 1
+for i in range(n):
+    product = product * (i + 1)
+print(product)
+
