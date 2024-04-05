ff59c1155e6b82b461910b023f4fd2bb3f6d9204
Ankur Konan <112815485+AnkurKonan@users.noreply.github.com>
2024-04-06 00:24:27 +0530

    Create P27.py

---
+++ Practice/P27.py
@@ -0,0 +1,42 @@
+def maximum(n1, n2,n3):
+    if (n1 > n2 & n1 > n3):
+        return n1
+    elif(n2 > n3 & n2 > 1):
+        return n2
+    else:
+        return n3
+print(maximum(3423,5634,7556))
+
+def maximum1(num1, num2, num3):
+    if num1 > num2:
+        if num1 > num3:
+            return num1
+        else:
+            return num3
+    else:
+        if num2 > num3:
+            return num2
+        else:
+            return num3
+m = maximum1(13343, 5534, 234)
+print("The value of the maximum is " + str(m))
+
+
+a = int(input("Enter 1st number: "))
+b = int(input("Enter 2nd number: "))
+c = int(input("Enter 3rd number: "))
+d = int(input("Entre 4th number: "))
+if a > b and a > c and a > d:
+  print('the largest number' + str(a))
+elif a < b and a < c and a < d:
+  print('the smallest number' + str(a))
+elif b > a and b > c and b > d:
+  print('the largest number' + str(b))
+elif b < a and b < c and b < d:
+  print('the smallest number is' + str(b))
+elif d > a and d > b and d > c:
+  print('the biggest number is' + str(d))
+elif d < a and d < b and d < c:
+  print('the smallest number is'+ str(d))
+  
+
