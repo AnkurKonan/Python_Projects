61f99919d6ed106746e153acf5508795ef422344
Ankur Konan <112815485+AnkurKonan@users.noreply.github.com>
2024-07-10 17:58:53 +0530

    Update O6.py

--- OpenCV/O6.py
+++ OpenCV/O6.py
@@ -14,10 +14,10 @@ cv2.imshow('Blank Image', blank)
 circle = cv2.circle(blank.copy(), (resized_img.shape[1] // 2 + 45, resized_img.shape[0] // 2), 100, 255, -1)
 rectangle = cv2.rectangle(blank.copy(), (30, 30), (370, 370), 255, -1)
 
-weird_shape = cv2.bitwise_and(circle, rectangle)
-cv2.imshow('Half cut circle', weird_shape)
+half_circle = cv2.bitwise_and(circle, rectangle)
+cv2.imshow('Half cut circle', half_circle)
 
-masked = cv2.bitwise_and(resized_img, resized_img, mask=weird_shape)
+masked = cv2.bitwise_and(resized_img, resized_img, mask=half_circle)
 cv2.imshow('Half cut masked image', masked)
 cv2.waitKey(100000)
 cv2.destroyAllWindows()
