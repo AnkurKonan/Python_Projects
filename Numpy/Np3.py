01216bf8bbf0214666f2bd66cedba27de68dda5d
Ankur Konan <112815485+AnkurKonan@users.noreply.github.com>
2024-07-12 23:25:43 +0530

    Create Np3.py

---
+++ Numpy/Np3.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+a = np.array([1,2,3,4])
+b = np.array([1,0,1,0])
+print(a)
+print(a + 2)
+print(a - 2)
+print(a * 2)
+print(a / 2)
+print(a + b)
+print(a ** 2)
+print(np.cos(a))
+print(np.sin(a))
+
+a = np.ones((2,3))
+print(a)
+b = np.full((3,2), 2)
+print(b)
+np.matmul(a,b)
+
+c = np.identity(3)
+np.linalg.det(c)
