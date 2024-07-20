fe6fa32a6810d73969f8edf9111ea77359fd21e6
Ankur Konan <112815485+AnkurKonan@users.noreply.github.com>
2024-07-20 14:32:16 +0530

    Update Mp10.py

--- Matplotlib/Mp10.py
+++ Matplotlib/Mp10.py
@@ -7,3 +7,11 @@ fig = plt.figure(figsize=(8,6))
 ax = fig.add_subplot(111, projection='3d')
 ax.scatter(df[:99].depth,df[:99].table,df[:99].price)
 plt.show()
+
+#Random dots
+import numpy as np
+import matplotlib.pyplot as plt
+x = np.random.random(50) * 100
+y = np.random.random(50) * 100
+plt.scatter(x,y)
+plt.show()
